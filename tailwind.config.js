f9efdbdc6b48c02bf1ea40290c0f925f352d070c
Julia T <juliaturkali@gmail.com>
2025-01-08 00:47:59 +0100

    Include Colors in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBg: "#1E2021",
+        lightGray: "#4F5E55",
+        darkGray: "#272B3F",
+        clearOrange: "#CF6E21",
+        pastelGreen: "#65B70D",
+      },
+    },
   },
   plugins: [],
 };
